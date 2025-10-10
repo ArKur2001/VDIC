a809162ef19314517da2f54d1c2707dc02b4fc36
ArKur2001 <arekkurnik006@wp.pl>
2025-10-10 14:27:33 +0200

    tb added

---
+++ lab01/run/INCA_libs/run.lnx8664.24.09.d/.timestamp.ts
@@ -0,0 +1,2 @@
+1760080842 /home/student/akurnik/VDIC/common/simple_uart_switch.svp
+1760098954 /home/student/akurnik/VDIC/lab01/tb/simple_uart_switch_tb.svp
---
+++ lab01example/run/INCA_libs/run.lnx8664.24.09.d/.timestamp.ts
@@ -0,0 +1,5 @@
+1728048935 /home/student/akurnik/VDIC/common/tinyalu/tinyalu.sv
+1696515437 /home/student/akurnik/VDIC/common/tinyalu/three_cycle.sv
+1728048979 /home/student/akurnik/VDIC/common/tinyalu/single_cycle.sv
+1729592579 /home/student/akurnik/VDIC/common/tinyalu/tinyalu_pkg.sv
+1697728271 /home/student/akurnik/VDIC/lab01example/tb/tinyalu_tb.sv

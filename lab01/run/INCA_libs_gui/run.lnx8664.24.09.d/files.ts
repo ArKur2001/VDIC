e7e489f596f5fe136eecc6ea84a259cc90b8a2ad
ArKur2001 <arekkurnik006@wp.pl>
2025-10-14 22:04:58 +0200

    task_receiver_done

--- lab01/run/INCA_libs_gui/run.lnx8664.24.09.d/files.ts
+++ lab01/run/INCA_libs_gui/run.lnx8664.24.09.d/files.ts
@@ -1,2 +1,2 @@
 1760080842 /home/student/akurnik/VDIC/common/simple_uart_switch.svp
-1760460405 /home/student/akurnik/VDIC/lab01/tb/simple_uart_switch_tb.svp
+1760472199 /home/student/akurnik/VDIC/lab01/tb/simple_uart_switch_tb.svp

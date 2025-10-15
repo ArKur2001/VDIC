28e02bcb70eb22a859a196a2f96db18c1de23603
ArKur2001 <arekkurnik006@wp.pl>
2025-10-16 00:04:14 +0200

    TEST cases done

--- lab01/run/INCA_libs/run.lnx8664.24.09.d/files.ts
+++ lab01/run/INCA_libs/run.lnx8664.24.09.d/files.ts
@@ -1,2 +1,2 @@
 1760080842 /home/student/akurnik/VDIC/common/simple_uart_switch.svp
-1760468813 /home/student/akurnik/VDIC/lab01/tb/simple_uart_switch_tb.svp
+1760565719 /home/student/akurnik/VDIC/lab01/tb/simple_uart_switch_tb.svp

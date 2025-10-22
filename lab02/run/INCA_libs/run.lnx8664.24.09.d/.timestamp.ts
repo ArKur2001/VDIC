2ecfd1ac78a283b047ddb678d30cb01e8630ca25
ArKur2001 <arekkurnik006@wp.pl>
2025-10-22 10:45:39 +0200

    covergroup in progress

--- lab02/run/INCA_libs/run.lnx8664.24.09.d/.timestamp.ts
+++ lab02/run/INCA_libs/run.lnx8664.24.09.d/.timestamp.ts
@@ -1,2 +1,2 @@
 1760080842 /home/student/akurnik/VDIC/common/simple_uart_switch.svp
-1760996710 /home/student/akurnik/VDIC/lab02/tb/simple_uart_switch_tb.svp
+1761122387 /home/student/akurnik/VDIC/lab02/tb/simple_uart_switch_tb.svp
---
+++ lab02example/run/INCA_libs/run.lnx8664.24.09.d/files.ts
@@ -0,0 +1,5 @@
+1729592579 /home/student/akurnik/VDIC/common/tinyalu/tinyalu_pkg.sv
+1728048979 /home/student/akurnik/VDIC/common/tinyalu/single_cycle.sv
+1696515437 /home/student/akurnik/VDIC/common/tinyalu/three_cycle.sv
+1728048935 /home/student/akurnik/VDIC/common/tinyalu/tinyalu.sv
+1760699226 /home/student/akurnik/VDIC/lab02example/tb/tinyalu_tb.sv

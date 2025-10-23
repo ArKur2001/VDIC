6bac5eb5c12a23604adf2b58b58146d6ba661ec6
ArKur2001 <arekkurnik006@wp.pl>
2025-10-23 22:25:11 +0200

    lab 2 done

--- lab02/run/INCA_libs/run.lnx8664.24.09.d/files.ts
+++ lab02/run/INCA_libs/run.lnx8664.24.09.d/files.ts
@@ -1,2 +1,2 @@
 1760080842 /home/student/akurnik/VDIC/common/simple_uart_switch.svp
-1761149165 /home/student/akurnik/VDIC/lab02/tb/simple_uart_switch_tb.svp
+1761250758 /home/student/akurnik/VDIC/lab02/tb/simple_uart_switch_tb.svp

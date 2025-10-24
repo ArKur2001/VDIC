fb5ea926e3ac1e553f2956c48a5edb8c7f57780b
ArKur2001 <arekkurnik006@wp.pl>
2025-10-24 14:37:15 +0200

    lab 2 done

--- lab02/run/INCA_libs/run.lnx8664.24.09.d/files.ts
+++ lab02/run/INCA_libs/run.lnx8664.24.09.d/files.ts
@@ -1,2 +1,2 @@
 1760080842 /home/student/akurnik/VDIC/common/simple_uart_switch.svp
-1761250758 /home/student/akurnik/VDIC/lab02/tb/simple_uart_switch_tb.svp
+1761308909 /home/student/akurnik/VDIC/lab02/tb/simple_uart_switch_tb.svp
--- lab02/run/INCA_libs_gui/run.lnx8664.24.09.d/.timestamp.ts
+++ lab02/run/INCA_libs_gui/run.lnx8664.24.09.d/.timestamp.ts
@@ -1,2 +1,2 @@
 1760080842 /home/student/akurnik/VDIC/common/simple_uart_switch.svp
-1760993564 /home/student/akurnik/VDIC/lab02/tb/simple_uart_switch_tb.svp
+1761308307 /home/student/akurnik/VDIC/lab02/tb/simple_uart_switch_tb.svp

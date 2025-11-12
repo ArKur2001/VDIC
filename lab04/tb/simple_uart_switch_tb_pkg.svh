f71f7da02305ea4892c7bbfd068f36f9a09b9fe3
ArKur2001 <arekkurnik006@wp.pl>
2025-11-12 11:09:56 +0100

    lab04 init

---
+++ lab04/tb/simple_uart_switch_tb_pkg.svh
@@ -0,0 +1,27 @@
+package simple_uart_switch_tb_pkg;
+
+  const int CLKS_PER_BIT = 16;
+  const int FRAME_LENGTH = 11;
+  const byte PORT_SOUT0 = 8'h00;
+  const byte PORT_SOUT1 = 8'h01;
+
+  typedef struct{
+    logic reset;
+    logic prog_mode;
+    logic [10:0] input_message;
+  } frame_data_t;
+
+  typedef enum logic[2:0] {
+    PROPER_FRAME = 3'b000,
+    WRONG_START_BIT = 3'b001,
+    WRONG_PARITY_BIT = 3'b010,
+    WRONG_STOP_BIT = 3'b011,
+    WRONG_START_PARITY_STOP = 3'b100,
+    RESET = 3'b111
+  } test_t;
+
+  typedef enum bit {
+    TEST_IN_PROGRESS,
+    TEST_END
+  } test_end_t;
+endpackage
---
+++ lab04example/run/INCA_libs/run.lnx8664.24.09.d/.timestamp.ts
@@ -0,0 +1,7 @@
+1728048935 /home/student/akurnik/VDIC/common/tinyalu/tinyalu.sv
+1696515437 /home/student/akurnik/VDIC/common/tinyalu/three_cycle.sv
+1730812189 /home/student/akurnik/VDIC/lab04example/tb/tinyalu_bfm.sv
+1728048979 /home/student/akurnik/VDIC/common/tinyalu/single_cycle.sv
+1730811878 /home/student/akurnik/VDIC/lab04example/tb/top.sv
+1729592579 /home/student/akurnik/VDIC/common/tinyalu/tinyalu_pkg.sv
+1730812117 /home/student/akurnik/VDIC/lab04example/tb/tinyalu_tb_pkg.svh
